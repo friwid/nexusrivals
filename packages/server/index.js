971761522d51be8d3655666e184f3466f52d10bb
friwid <50137120+friwid@users.noreply.github.com>
2024-02-16 14:54:04 +0100

    Change server file structure, add client route

--- packages/server/index.js
+++
@@ -1,7 +0,0 @@
-Bun.serve({
-  fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname === "/") return new Response("Home page!");
-    return new Response("404!");
-  },
-});
---
+++ packages/server/src/server.js
@@ -0,0 +1,21 @@
+import Fastify from 'fastify';
+import path from 'path';
+import fastifyStatic from '@fastify/static';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Verwenden Sie path.resolve, um den absoluten Pfad zu erhalten
+fastify.register(fastifyStatic, {
+  root: path.resolve(__dirname, '../../client/dist/'),
+  prefix: '/',
+});
+
+
+try {
+  fastify.listen({ port: 3000 });
+} catch (error) {
+  fastify.log.error(error);
+  process.exit(1);
+} 
