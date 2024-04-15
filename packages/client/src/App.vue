22e4110eb3bb815fe6a24f35142c2e84b1a11e08
friwid <50137120+friwid@users.noreply.github.com>
2024-04-15 15:21:06 +0200

    Rename component Navbar to MainNavbar

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,26 +1,25 @@
 <script setup>
-import Navbar from "./components/Navbar.vue";
-import RegisterDialog from "./components/RegisterDialog.vue";
-import LoginDialog from "./components/LoginDialog.vue";
-import { inject, computed } from "vue";
-import { useTheme } from "vuetify";
+import MainNavbar from './components/MainNavbar.vue'
+import RegisterDialog from './components/RegisterDialog.vue'
+import LoginDialog from './components/LoginDialog.vue'
+import { inject, computed } from 'vue'
+import { useTheme } from 'vuetify'
 
-const bus = inject("$bus");
-const localStorage = inject("$localStorage");
+const bus = inject('$bus')
+const localStorage = inject('$localStorage')
 
 // Get and set theme in localStorage
-const theme = useTheme();
-theme.global.name.value = localStorage.$getItem("theme") || theme.global.name.value;
-bus.$on("changeTheme", () => {
-  theme.global.name.value =
-  theme.global.name.value === "dark" ? "light" : "dark";
-  localStorage.$setItem("theme", theme.global.name.value);
-});
+const theme = useTheme()
+theme.global.name.value = localStorage.$getItem('theme') || theme.global.name.value
+bus.$on('changeTheme', () => {
+  theme.global.name.value = theme.global.name.value === 'dark' ? 'light' : 'dark'
+  localStorage.$setItem('theme', theme.global.name.value)
+})
 </script>
 
 <template>
   <v-app>
-    <Navbar></Navbar>
+    <MainNavbar />
     <v-main>
       <RouterView></RouterView>
       <RegisterDialog></RegisterDialog>
