58b3809472b8974a244fa6aca993afdb6e7a7914
friwid <50137120+friwid@users.noreply.github.com>
2024-04-29 19:02:29 +0200

    Add folder and file structure for Elysia and test

---
+++ packages/server/src/types/types.ts
@@ -0,0 +1,24 @@
+export type EventBus = {
+  $on(eventName: string, fn: (data?: object) => void): void
+  $emit(eventName: string, data?: object): void
+  $off(eventName: string, fn: (data?: object) => void): void
+}
+
+export type LocalStorage = {
+  $setItem(key: string, value: string): void
+  $getItem(key: string): string | void | null
+}
+export type FormRule = true | string
+
+export type LoginData = {
+  username: string
+  password: string
+  isRememberMe: boolean
+}
+
+export type RegisterData = {
+  username: string
+  email: string
+  password: string
+  isTerms: boolean
+}
