f626889b4c000f3c06baded49d3fff65a71544dd
friwid <50137120+friwid@users.noreply.github.com>
2024-04-29 11:37:18 +0200

    Fix TS errors for vite environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENV_DEBUG: string
+  readonly VITE_API_URL: string
+  readonly VITE_USER_NODE_ENV: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
