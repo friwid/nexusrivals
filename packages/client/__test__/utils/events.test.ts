671898b034ff48d4bf50bd414c5bd1a9877d7554
friwid <50137120+friwid@users.noreply.github.com>
2024-05-01 21:30:00 +0200

    Add rudimentary unit test

--- packages/client/__test__/utils/events.test.ts
+++ packages/client/__test__/utils/events.test.ts
@@ -1,16 +1,58 @@
-import { beforeAll, afterAll, test, expect } from 'bun:test'
+import { beforeAll, afterAll, beforeEach, afterEach, test, expect, describe, mock } from 'bun:test'
 
-import bus from '../../src/utils/events.ts'
+import $bus from '../../src/utils/events.ts'
 
 beforeAll(() => {
-  console.log('\n▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏')
-  console.log('▕ Test Event Bus                                           ▏')
-  console.log('▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏\n')
-  console.log('▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏')
+  console.log(
+    '\n▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏'
+  )
+  console.log('▕  🧪  Test Event Bus')
+  console.log(
+    '▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏\n'
+  )
+  console.log(
+    '▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏'
+  )
 })
 
-test.todo('$on, $off and $emit', () => {})
+afterEach(() => {
+  // Clear all events after each test
+  $bus.$off = originalBusOff
+  $bus.$on = originalBusOn
+  $bus.$emit = originalBusEmit
+})
 
 afterAll(() => {
-  console.log('▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏')
+  console.log(
+    '▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏\n'
+  )
 })
+
+const originalBusOff = $bus.$off
+const originalBusOn = $bus.$on
+const originalBusEmit = $bus.$emit
+
+test('Register an event', () => {
+  $bus.$on = mock()
+  const eventName = 'testEvent'
+  const fn = mock()
+  $bus.$on(eventName, fn)
+  expect($bus.$on).toHaveBeenCalledWith(eventName, fn)
+})
+
+test('Emit an event', () => {
+  const eventName = 'testEvent'
+  const fn = mock()
+  $bus.$on(eventName, fn)
+  $bus.$emit(eventName)
+  expect(fn).toHaveBeenCalled()
+})
+
+test('Not emit an event if not registered', () => {
+  const eventName = 'testEvent'
+  const fn = mock()
+  $bus.$emit(eventName)
+  expect(fn).not.toHaveBeenCalled()
+})
+
+test.todo('$off', () => {})
