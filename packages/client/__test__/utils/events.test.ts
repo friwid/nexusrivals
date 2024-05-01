4cec22ca5c5151452f0232e839c99f5427a832e0
friwid <50137120+friwid@users.noreply.github.com>
2024-05-01 02:22:09 +0200

    Add initial events.test.ts without tests

---
+++ packages/client/__test__/utils/events.test.ts
@@ -0,0 +1,16 @@
+import { beforeAll, afterAll, test, expect } from 'bun:test'
+
+import bus from '../../src/utils/events.ts'
+
+beforeAll(() => {
+  console.log('\n▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏')
+  console.log('▕ Test Event Bus                                           ▏')
+  console.log('▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏\n')
+  console.log('▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏')
+})
+
+test.todo('$on, $off and $emit', () => {})
+
+afterAll(() => {
+  console.log('▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏')
+})
