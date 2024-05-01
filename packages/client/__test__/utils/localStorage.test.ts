7dff9f423a9cfc053b2c5e2a4056c8cd51f31c77
friwid <50137120+friwid@users.noreply.github.com>
2024-05-01 02:21:01 +0200

    Add $removeItem to localStorage function

--- packages/client/__test__/utils/localStorage.test.ts
+++ packages/client/__test__/utils/localStorage.test.ts
@@ -1,5 +1,4 @@
 import { beforeAll, afterAll, test, expect } from 'bun:test'
-
 import localStorage from '../../src/utils/localStorage.ts'
 
 beforeAll(() => {
@@ -9,10 +8,23 @@ beforeAll(() => {
   console.log('▕▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▏')
 })
 
-test('$setItem and $getItem', () => {
-  localStorage.$setItem('testkey', 'testvalue')
+localStorage.$setItem('testkey', 'testvalue')
+
+test('$getItem for existing key', () => {
   expect(localStorage.$getItem('testkey')).toStrictEqual('testvalue')
 })
+
+test('$getItem for non-existing key', () => {
+  expect(localStorage.$getItem('wrongkey')).toStrictEqual(null)
+})
+
+test('$removeItem for existing key', () => {
+  localStorage.$setItem('removekey', 'removevalue')
+  localStorage.$removeItem('removekey')
+  expect(localStorage.$getItem('removekey')).toStrictEqual(null)
+})
+
+test.todo('clear(): Clear all LocalStorage')
 
 afterAll(() => {
   console.log('▕▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▏')
--- packages/client/src/utils/localStorage.ts
+++ packages/client/src/utils/localStorage.ts
@@ -15,7 +15,7 @@ export default {
   $setItem(key, value) {
     isLocalStorageAvailable()
       ? localStorage.setItem(key, JSON.stringify(value))
-      : console.error(
+      : console.log(
           `Can't $setItem([key=${key}], [value=${value}]): isLocalStorageAvailable returned false.`
         )
   },
@@ -30,7 +30,21 @@ export default {
         return null
       }
     } else {
-      return console.error(`Can't $getItem([key=${key}]): isLocalStorageAvailable returned false.`)
+      return console.log(`Can't $getItem([key=${key}]): isLocalStorageAvailable returned false.`)
+    }
+  },
+  $removeItem(key) {
+    if (isLocalStorageAvailable()) {
+      try {
+        localStorage.removeItem(key)
+      } catch (error) {
+        console.error('Error while removing key:', error)
+        return null
+      }
+    } else {
+      return console.error(
+        `Can't $removeItem([key=${key}]): isLocalStorageAvailable returned false.`
+      )
     }
   }
 } as LocalStorage
